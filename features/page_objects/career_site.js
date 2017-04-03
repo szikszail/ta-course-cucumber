cadef1d5987ee025c2a0d05c196eb8cbd79ae91d
Dora_Vereb <dora.vereb@wolterskluwer.com>
2017-04-03 10:55:14 +0200

    Final

--- features/page_objects/career_site.js
+++ features/page_objects/career_site.js
@@ -1,6 +1,93 @@
 function Site() {
 
-    
+    var LOCATION_FILTER_BOX = '.select-box',
+        ROLES_FILTER_BOX = '.multi-select-filter',
+        SEARCH_BUTTON = '.career-apply-box',
+        FIND_YOUR_DREAM_JOB = '.job-search-title',
+        DESCRIPTION = '.recruiting-details-description-header';
+
+    this.getSite = function () {
+        return driver.get('https://www.epam.com/careers');
+    };
+
+    this.isSloganVisible = function () {
+        return driver.isElementVisible(by.css(FIND_YOUR_DREAM_JOB));
+    };
+
+    this.clickLocationFilterBox = function () {
+        return driver.findElement(by.css(LOCATION_FILTER_BOX)).click();
+    };
+
+    this.clickRoleFilterBox = function () {
+        return driver.findElement(by.css(ROLES_FILTER_BOX)).click();
+    };
+
+    this.selectCountry = function (country) {
+        var cnt = driver.findElement(by.xpath('//li[contains(@class,"option")]/strong[(.)="' + country + '"]'));
+        return cnt.getAttribute('class').then(function (text) {
+            if (text.indexOf('dropdown') == -1) {
+                return cnt.click();
+            }
+        });
+    };
+
+    this.selectCity = function (city) {
+        return driver.findElement(by.xpath('//li[contains(@class,"option")]/ul[contains(@class,"options")]/li[contains(.,"' + city + '")]')).click();
+    };
+
+    this.selectRole = function (role) {
+        return driver.findElement(by.xpath('//ul[contains(@class,"multi-select-column")]/li/label/span[contains(., "' + role + '")]')).click();
+    };
+
+    this.clickSearchButton = function () {
+        return driver.findElement(by.css(SEARCH_BUTTON)).click();
+    };
+
+    this.isPositionNameVisible = function (position) {
+        return driver.findElement(by.xpath(
+            '//li[@class="search-result-item"]/div/a[.="' + position + '"]'
+        )).isDisplayed();
+    };
+
+    this.getCategoryText = function (position) {
+        return driver.findElement(by.xpath(
+            '//li[@class="search-result-item"][./div/a[.="' + position + '"]]/div[@class="department"]'
+        )).getText();
+    };
+
+    this.isPriorityHigh = function (position) {
+        return driver.findElement(by.xpath(
+            '//li[@class="search-result-item"]/div[./a[.="' + position + '"]]'
+        )).getAttribute('class').then(function (attr) {
+            return attr.indexOf('hot') > -1;
+        });
+    };
+
+    this.getLocationText = function (position) {
+        return driver.findElement(by.xpath(
+            '//li[@class="search-result-item"][./div/a[.="' + position + '"]]/div[@class="location"]'
+        )).getText().then(function (text) {
+            return text.replace('&ensp;', ' ');
+        });
+    };
+
+    this.isApplyButtonVisible = function (position) {
+        return driver.findElement(by.xpath(
+            '//li[@class="search-result-item"][./div/a[.="' + position + '"]]/div[@class="button-apply"]/a'
+        )).isDisplayed();
+    };
+
+    this.clickApplyButton = function (position) {
+        return driver.findElement(by.xpath(
+            '//li[@class="search-result-item"][./div/a[.="' + position + '"]]/div[@class="button-apply"]/a'
+        )).click();
+    };
+
+    this.isDescriptionVisible = function (text) {
+        return driver.findElement(by.css(DESCRIPTION)).getText().then(function (desc) {
+            return desc.indexOf(text) > -1;
+        });
+    };
 }
 
 module.exports = new Site();
--- features/step_definitions/career_site.js
+++ features/step_definitions/career_site.js
@@ -2,6 +2,59 @@ module.exports = function () {
 
     var Site = require('../page_objects/career_site');
 
-    
+    this.Given(/^the EPAM career page is opened$/, function () {
+        Site.getSite();
+        return driver.wait(function () {
+            return Site.isSloganVisible();
+        });
+    });
+
+    this.When(/^the (Location|Teams and Roles) filter box is clicked$/, function (box) {
+        if (box == 'Location') {
+            return Site.clickLocationFilterBox();
+        }
+        return Site.clickRoleFilterBox();
+    });
+
+    this.When(/^the (country|city|role) "([^"].*)" is selected$/, function (type, name) {
+        if (type == 'country') {
+            return Site.selectCountry(name);
+        } else if (type == 'city') {
+            return Site.selectCity(name);
+        }
+        return Site.selectRole(name);
+    });
+
+    this.When(/^the Search button is clicked$/, function () {
+        return Site.clickSearchButton();
+    });
+
+    this.Then(/^the position "([^"].*)" should be (visible|hidden)$/, function (name, visibility) {
+        return expect(Site.isPositionNameVisible(name)).to.eventually.equal(visibility === 'visible');
+    });
+
+    this.Then(/^the priority of the position "([^"].*)" should be (normal|high)$/, function (position, prio) {
+        return expect(Site.isPriorityHigh(position)).to.eventually.equal(prio == 'high');
+    });
+
+    this.Then(/^the (category|location) of "([^"].*)" position should be "([^"].*)"$/, function (type, position, text) {
+        if (type == 'category') {
+            return expect(Site.getCategoryText(position)).to.eventually.equal(text);
+        }
+        return expect(Site.getLocationText(position)).to.eventually.equal(text);
+    });
+
+    this.Then(/^the Apply button for "([^"].*)" position should be (visible|hidden)$/, function (position, visibility) {
+        return expect(Site.isApplyButtonVisible(position)).to.eventually.equal(visibility == 'visible');
+    });
+
+    this.When(/^the Apply button for "([^"].*)" position is clicked$/, function (position) {
+        return Site.clickApplyButton(position);
+    });
+
+    this.Then(/^the job description should contain the following text: "([^"].*)"$/, function (text) {
+        return expect(Site.isDescriptionVisible(text)).to.eventually.be.true;
+    });
+
 };
 
