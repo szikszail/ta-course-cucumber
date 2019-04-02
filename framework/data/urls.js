7b2c88400d2e4263c1c55810f994025bc65e3fd4
sandor_orosz <SsamcroO#8989>
2019-04-02 16:11:56 +0200

    add po

---
+++ framework/data/urls.js
@@ -0,0 +1,5 @@
+module.exports = urls = {
+    base: "https://www.epam.com",
+    careerSite: "https://www.epam.com/careers",
+    jobListing: "https://www.epam.com/careers/job-listings"
+}
---
+++ po/selectors/home.js
@@ -0,0 +1,6 @@
+module.exports = {
+    cookieButton: '.cookie-disclaimer button',
+    sliderSlide: '.slider__slide',
+    locationFilterBox: '.recruiting-search__location',
+    jobFilterBox: '.job-search__departments'
+}
